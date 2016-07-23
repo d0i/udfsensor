8fff3d42ee3f7b2ec7e93807f762baa380bcd4be
Yusuke DOI <yusuke.doi@gmail.com>
2016-07-23 14:19:39 +0900

    fixed intentional and unintentional bugs

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -1,7 +1,7 @@
 package plugin
 
 import (
-	"github.com/d0i/udfsensor.git"
+	"github.com/d0i/udfsensor"
 	"gopkg.in/sensorbee/sensorbee.v0/bql"
 	"gopkg.in/sensorbee/sensorbee.v0/bql/udf"
 	"fmt"
@@ -11,6 +11,5 @@ func init() {
 	udf.MustRegisterGlobalUDSFCreator("us_sensor",
 		udf.MustConvertToUDSFCreator(udfsensor.CreateSensor))
 	fmt.printf("registered us_sensor")
-	panic()
 }
 	
