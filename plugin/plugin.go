53c5443ff1b17e0087307c8194fbd4a4b7878410
Yusuke DOI <yusuke.doi@gmail.com>
2016-07-23 15:37:44 +0900

    now working as dummy sensor.

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -2,14 +2,14 @@ package plugin
 
 import (
 	"github.com/d0i/udfsensor"
-	_ "gopkg.in/sensorbee/sensorbee.v0/bql"
-	"gopkg.in/sensorbee/sensorbee.v0/bql/udf"
+	"gopkg.in/sensorbee/sensorbee.v0/bql"
+	_ "gopkg.in/sensorbee/sensorbee.v0/bql/udf"
 	"fmt"
 )
 
 func init() {
-	udf.MustRegisterGlobalUDSFCreator("us_sensor",
-		udf.MustConvertToUDSFCreator(udfsensor.CreateSensor))
+        bql.MustRegisterGlobalSourceCreator("us_sensor",
+		bql.SourceCreatorFunc(udfsensor.CreateSensor))
 	fmt.Println("registered us_sensor")
 }
 	
--- udfsensor.go
+++ udfsensor.go
@@ -4,22 +4,33 @@ import (
 	"gopkg.in/sensorbee/sensorbee.v0/bql"
 	"gopkg.in/sensorbee/sensorbee.v0/core"
 	"gopkg.in/sensorbee/sensorbee.v0/data"
+	"math/rand"
+	"math"
 	"time"
 )
 
 
 // as data source of SensorBee
+// XXX ここ，元のsampleにならってdata.Floatにしたけど，生のfloat64とかではNGなの？
 type Sensor struct {
 	interval time.Duration
 	average data.Float
+	stddev data.Float // fluctuation
+	cycle_depth data.Float // cycle emulated by math.Sin
 }
 
 // GenerateStream generates random sensor value
 // XXX これの元定義が見つけられない...
 func (this *Sensor) GenerateStream(ctx *core.Context, w core.Writer) error {
-	// first, this is a broken sensor
+	// simple sensor
 	for {
-		t := core.NewTuple(data.Map{"value": this.average})
+		ut := float64(time.Now().Unix())/12.0 // no reason for 12.0
+		var v data.Float
+		v = this.average + this.cycle_depth * data.Float(math.Sin(ut)) + this.stddev * data.Float(rand.NormFloat64())
+		t := core.NewTuple(data.Map{
+			"type": data.String("temp"),
+			"value": v,
+		})
 		if err := w.Write(ctx, t); err != nil {
 			return err
 		}
@@ -35,7 +46,9 @@ func (this *Sensor) Stop(ctx *core.Context) error {
 func CreateSensor(ctx *core.Context, ioParams *bql.IOParams, params data.Map) (core.Source, error) {
 	interval := 100 * time.Millisecond
 	average := data.Float(28.5)
-	if v, ok := params["iterval"]; ok {
+	stddev := data.Float(0.3)
+	cycle_depth := data.Float(5.0)
+	if v, ok := params["interval"]; ok {
 		i, err := data.ToDuration(v)
 		if err != nil {
 			return nil, err
@@ -45,5 +58,7 @@ func CreateSensor(ctx *core.Context, ioParams *bql.IOParams, params data.Map) (c
 	return core.ImplementSourceStop(&Sensor{
 		interval: interval,
 		average: average,
+		stddev: stddev,
+		cycle_depth: cycle_depth,
 	}), nil
 }
